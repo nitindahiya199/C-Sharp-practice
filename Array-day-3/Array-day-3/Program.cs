d229c3a4322bbb2c188d1c8fe1d21ba331fcb0d6
Nitin Dahiya <99438990+nitindahiya199@users.noreply.github.com>
2023-08-30 14:13:18 +0530

    Add files via upload

---
+++ Array-day-3/Array-day-3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_day_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // int[] num = new int[5];
+            // num[0] = 1;
+            // num[1] = 2;
+            // num[2] = 4;
+            // num[3] = 8;
+            // num[4] = 16;
+            // 
+            // Console.WriteLine(num[0]);
+            // Console.WriteLine(num.Length);
+            // Console.ReadLine();
+
+
+
+            // int[] num = new int[] { 10, 2, };
+            // 
+            // for (int i = 0; i < num.Length; i++) {
+            //     Console.WriteLine(num[i]);
+            // }
+            // 
+            // foreach (var item in num)
+            // {
+            //     Console.WriteLine(item);
+            // }
+            // 
+            // Console.WriteLine(num[0]);
+            // Console.ReadLine();
+
+
+
+            string name = "hey my name is Nitin" + " and i am learning C# ";
+
+            char[] chars = name.ToCharArray();
+            Array.Reverse(chars);
+
+            foreach (var item in chars)
+            {
+                Console.Write(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
