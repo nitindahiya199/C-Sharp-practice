6cbeab529e3d5ba80caf5fb8dac7720cc531a470
Nitin Dahiya <99438990+nitindahiya199@users.noreply.github.com>
2023-08-30 14:12:43 +0530

    Add files via upload

---
+++ Private-methods-day-3/Private-methods-day-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_methods_day_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
